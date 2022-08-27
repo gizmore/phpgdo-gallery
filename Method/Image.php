c329fe263333ebaf8dfce66f1a70985552f7f2da
gizmore <gizmore@wechall.net>
2022-08-27 19:32:06 +0200

    '7.0.1-r1542'

--- Method/Image.php
+++ Method/Image.php
@@ -6,13 +6,15 @@ use GDO\Gallery\GDO_GalleryImage;
 use GDO\Util\Common;
 use GDO\File\Method\GetFile;
 use GDO\User\GDO_User;
-use GDO\Core\GDT_String;
+use GDO\Core\GDT_Object;
 
 /**
  * Download a gallery image.
+ * Checks view permission.
+ * 
  * @author gizmore@wechall.net
- * @version 6.08
- * @since 6.04
+ * @version 7.0.1
+ * @since 6.4.0
  */
 final class Image extends Method
 {
@@ -21,37 +23,41 @@ final class Image extends Method
     public function gdoParameters() : array
     {
     	return [
-    		GDT_String::make('id')->notNull(),
+    		GDT_Object::make('id')->table(GDO_GalleryImage::table())->notNull(),
     	];
+    }
+    
+    public function getImage() : GDO_GalleryImage
+    {
+    	return $this->gdoParameterValue('id');
     }
     
     public function execute()
 	{
-		$fileId = $this->gdoParameterVar('id');
-		$image = GDO_GalleryImage::findBy('files_file', $fileId);
+    	$image = $this->getImage();
 		$gallery = $image->getGallery();
 		$reason = '';
 		if (!$gallery->canView(GDO_User::current(), $reason))
 		{
 			return $this->error('err_not_allowed', [$reason]);
 		}
-		return GetFile::make()->executeWithId($fileId, Common::getRequestString('variant'));
+		return GetFile::make()->executeWithId($image->getID(), Common::getRequestString('variant'));
 	}
 	
 	public function getMethodTitle() : string
 	{
-		if ($fileId = $this->gdoParameterVar('id'))
-		{
-			if ($image = GDO_GalleryImage::getBy('files_file', $fileId))
-			{
-				if ($descr = $image->displayDescription())
-				{
-					return $descr;
-				}
-				return t('mt_gallery_image', [$image->getID()]);
-			}
-		}
 		return t('mt_gallery_image', ['0']);
+// 		if ($fileId = $this->gdoParameterVar('id'))
+// 		{
+// 			if ($image = GDO_GalleryImage::getBy('files_file', $fileId))
+// 			{
+// 				if ($descr = $image->displayDescription())
+// 				{
+// 					return $descr;
+// 				}
+// 				return t('mt_gallery_image', [$image->getID()]);
+// 			}
+// 		}
 	}
 	
 }
